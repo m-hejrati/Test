1f69ee10c6b5f2cdc1d5daa103fcf25c838c9f9d
mahdi <60757625+m-hejrati@users.noreply.github.com>
2020-02-27 10:46:44 +0330

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,13 @@
+public class FirstClass {
+    private int myInt;
+
+    public void FirstClass (int i) {
+        myInt = i;
+    }
+
+
+    public int getMyInt() {
+        return myInt;
+    }
+}
+
